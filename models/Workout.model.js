30426daa8b32a627f215c6e8f9873af0824bccd3
João Pedrosa <jrcspp@hotmail.com>
2021-09-10 17:30:12 +0100

    api working

--- models/Workout.model.js
+++ models/Workout.model.js
@@ -1,27 +1,26 @@
 const mongoose = require("mongoose");
 
 const workoutSchema = mongoose.Schema(
-    {
-        title: String,
-        // exercise1: {
-        //     type: mongoose.Types.ObjectId,
-        //     ref: "exercise1"
-        // },
-        // exercise2: {
-        //     type: mongoose.Types.ObjectId,
-        //     ref: "exercise2"
-        // },
-        // exercise3: {
-        //     type: mongoose.Types.ObjectId,
-        //     ref: "exercise3"
-        // },      
-        exercise1: String,
-        exercise2: String,
-        exercise3: String,
-    },
-    {
-        timestamps: true,
-    }
+  {
+    title: String,
+
+    exercise: [
+      {
+        bodyPart: String,
+        equipment: String,
+        gifUrl: String,
+        id: String,
+        name: String,
+        target: String,
+      },
+    ],
+    // exercise1: String,
+    // exercise2: String,
+    // exercise3: String,
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const Workout = mongoose.model("Workout", workoutSchema);
