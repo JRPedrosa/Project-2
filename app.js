cc92e5dcd343fa32a30a8f4b9a63abb40cdb7df3
João Pedrosa <jrcspp@hotmail.com>
2021-09-13 10:48:35 +0100

    some organizing

--- app.js
+++ app.js
@@ -27,27 +27,13 @@ app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 // 👇 Start handling routes here
 const index = require("./routes/index");
 app.use("/", index);
+const workout = require("./routes/workout");
+app.use("/", workout)
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 
-/* var axios = require("axios").default;
-
-var options = {
-  method: 'GET',
-  url: 'https://exercisedb.p.rapidapi.com/exercises/equipment/%7Btype%7D',
-  headers: {
-    'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
-    'x-rapidapi-key': '5c69d9ad7fmsh37f71ce5cfc1d4dp1c82bfjsn3abce3514042'
-  }
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-}); */
 
 
 module.exports = app;
